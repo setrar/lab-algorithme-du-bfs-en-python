40b66d88998ea30905832eb8ef086857536cc5d1
b300098957 <300098957@collegeboreal.ca>
2020-03-29 22:40:01 -0400

    La taille de la queue est 4

--- b000000000.py
+++ b000000000.py
@@ -4,8 +4,32 @@
 @author: setrar
 """
 
-def main():
-  print('Informatique: le rêve')
+eleves = {}
+eleves["Boris"]=["Amir","Franck","Nathalie","Bertrand"]
+eleves["Amir"]=[]
+eleves["Franck"]=[]
+eleves["Nathalie"]=[]
+eleves["Bertrand"]=["Erna","Hassana","Abdelkrim"]
+eleves["Erna"]=[]
+eleves["Hassana"]=[]
+eleves["Zoureni"]=["Sekou","Auriane","Corlings"]
+eleves["Sekou"]=[]
+eleves["Auriane"]=[]
+eleves["Corlings"]=[]
+eleves["Abdelkrim"]=["Souleyman","Zack","Zoureni"]
+eleves["Souleyman"]=[]
+eleves["Zack"]=[]
+
+def personne_elue(name):
+    return name == 'Zoureni'
+
+from collections import deque
+
+def search(name):
+   search_queue = deque()
+   search_queue += eleves[name]
+   print( len(search_queue) )
+   return False
 
 if __name__== "__main__":
-  main()
+   search("Boris")
